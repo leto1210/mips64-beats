32163009837adef4660c1f1a0b2f3d5b352a1764
leto1210+Github <github@cparla.fr>
2019-02-17 22:11:13 +0100

    Create cp.sh

---
+++ cp.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#!/bin/bash
+echo "..:: Script to copy source files/directories to another target location ::.."
+echo "Supply the source file/directory below:"
+read srcName
+echo "Supply the target directory below:"
+read dstName
+echo " "
+echo "..::  Copying from: Source -> Destination  ::.."
+cp -rv -t "$dstName" "$srcName"
